39083690e28d20eab226128922146ced4e19753a
vossstef <stefan.voss@live.de>
2025-04-25 15:06:56 +0200

    fix

--- source/retroterm_univ.asm
+++ source/retroterm_univ.asm
@@ -3687,11 +3687,10 @@ udbase:
 	ORA #$31		;"1"
 	STA $0400+(6*40)+1		;Screen position for 1
 	TXA
-	EOR #$80
 	ORA #$32		;"2"
 	STA $0400+(7*40)+1		;Screen position for 2
 	TXA
-	EOR #$80
+	EOR #$3E
 	ORA #$33		;"3"
 	STA $0400+(8*40)+1		;Screen position for 3
 	RTS
